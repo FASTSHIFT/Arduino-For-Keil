d3a237e5de257d75337f3f36adf71aba6acb14da
_VIFEXTech <1290176185@qq.com>
2018-04-29 20:40:32 +0800

    Update pwm.h

--- Core/pwm.h
+++ Core/pwm.h
@@ -9,8 +9,8 @@ extern "C"{
 #include "Arduino.h"
 
 uint8_t PWM_Init(uint8_t Pin, uint16_t PWM_DutyCycle, uint16_t PWM_Frequency);
-uint16_t pwmWrite(uint8_t Pin,uint16_t val);
-void TIMx_Init(TIM_TypeDef* TIMx,uint16_t arr,uint16_t psc,uint8_t CHx);
+uint16_t pwmWrite(uint8_t Pin, uint16_t val);
+void TIMx_Init(TIM_TypeDef* TIMx, uint16_t arr, uint16_t psc, uint8_t CHx);
 
 uint16_t timer_get_compare(TIM_TypeDef* TIMx,u8 TimerChannel);
 void timer_set_prescaler(TIM_TypeDef* TIMx,uint16_t psc);
