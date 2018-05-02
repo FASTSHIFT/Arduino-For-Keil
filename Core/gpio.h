682388215a88d6b7f2c259a6f5021d69c0cb580e
_VIFEXTech <1290176185@qq.com>
2018-05-03 01:52:22 +0800

    Update gpio.h

--- Core/gpio.h
+++ Core/gpio.h
@@ -14,7 +14,7 @@ extern "C"{
 
 #define xADC 0xFF
 
-enum{
+typedef enum{
 	PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13, PA14, PA15,
 	PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15,
 	PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10, PC11, PC12, PC13, PC14, PC15,
@@ -22,7 +22,7 @@ enum{
 	PE0, PE1, PE2, PE3, PE4, PE5, PE6, PE7, PE8, PE9, PE10, PE11, PE12, PE13, PE14, PE15,
 	PF0, PF1, PF2, PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10, PF11, PF12, PF13, PF14, PF15,
 	PG0, PG1, PG2, PG3, PG4, PG5, PG6, PG7, PG8, PG9, PG10, PG11, PG12, PG13, PG14, PG15
-};
+}Pxx_TypeDef;
 
 typedef struct STM32_PinInfo{
 	GPIO_TypeDef* GPIOx;
@@ -32,9 +32,9 @@ typedef struct STM32_PinInfo{
 	uint16_t GPIO_Pin_x;	
 	uint8_t TimerChannel;	
 	uint8_t ADC_Channel;
-}STM32_PinInfo;
+}PinInfo_TypeDef;
 
-extern const STM32_PinInfo PIN_MAP[];
+extern const PinInfo_TypeDef PIN_MAP[];
 
 #define IS_ADC_PIN(Pin) (PIN_MAP[Pin].ADC_Channel != xADC)
 
