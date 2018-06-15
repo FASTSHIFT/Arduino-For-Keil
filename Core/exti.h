dc83dc20cbd6f07373400734937e7270ead17b81
_VIFEXTech <1290176185@qq.com>
2018-06-15 16:08:11 +0800

    Update exti.h

--- Core/exti.h
+++ Core/exti.h
@@ -8,6 +8,10 @@ extern "C"{
 #include "stm32f10x.h"
 #include "stm32f10x_exti.h"
 #include "Arduino.h"
+	
+#define	CHANGE EXTI_Trigger_Rising_Falling
+#define	FALLING EXTI_Trigger_Falling
+#define	RISING EXTI_Trigger_Rising
 
 void attach_EXTI(uint8_t Pin, void_func_point f, EXTITrigger_TypeDef Trigger_Mode, uint8_t PreemptionPriority, uint8_t SubPriority);
 void attachInterrupt(uint8_t Pin, void_func_point f, EXTITrigger_TypeDef Trigger_Mode);
