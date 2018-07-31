503d40930f6e1451261702c2d85d7cb195965796
_VIFEXTech <1290176185@qq.com>
2018-07-31 15:12:49 +0800

    Add files via upload

---
+++ _Keilduino (STM32F0xx)/Example/Basic.cpp
@@ -0,0 +1,62 @@
+#include "FileGroup.h"
+
+void setup()
+{
+	Serial.begin(115200);
+	Serial.printf("C++ Version: %d\r\n",__cplusplus);
+	Serial.printf("Compiling Time: %s %s\r\n",__DATE__,__TIME__);
+	pinMode(PC13,OUTPUT);
+	pinMode(PA0,PWM);
+}
+
+void loop()
+{
+	Serial.println("STM32 Serial Test!");
+	delay(2000);
+	
+	Serial.println("Digital Write TEST:");
+	for(int i=0;i<5;i++)
+	{
+		digitalWrite(PC13,HIGH);
+		delay(1000);
+		digitalWrite(PC13,LOW);
+		delay(1000);
+	}
+	
+	Serial.println("Digital Read TEST:");
+	pinMode(PC13,INPUT);
+	uint8_t State = digitalRead(PC13);
+	Serial.printf("PC13 State is:%d\r\n",State);
+	pinMode(PC13,OUTPUT);
+	
+	Serial.println("Analog Write TEST:");
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<=1000;j++)
+		{
+			analogWrite(PA0,j);
+			delay(1);
+		}
+		for(int j=1000;j>0;j--)
+		{
+			analogWrite(PA0,j);
+			delay(1);
+		}
+	}
+	
+	Serial.println("PA0 Analog Read TEST:");
+	pinMode(PA0,INPUT_ANALOG);
+	uint16_t ADC_Val = analogRead(PA0);
+	Serial.printf("PA0 ADC Val is:%d\r\n",ADC_Val);
+	pinMode(PA0,PWM);
+}
+
+//*****************MAIN******************//
+int main(void)
+{
+	Delay_Init();
+	ADCx_Init(ADC1);
+//	ADCx_DMA_Init();
+	setup();
+	for(;;)loop();
+}
---
+++ _Keilduino (STM32F0xx)/USER/FileGroup.h
@@ -0,0 +1,25 @@
+#ifndef __FILEGROUP_H
+#define	__FILEGROUP_H
+
+//*************C/C++ Library**************//
+
+#include "stm32f0xx.h"
+#include "Arduino.h"
+#include "delay.h"
+#include "exti.h"
+#include "gpio.h"
+#include "adc.h"
+#include "pwm.h"
+#include "usart.h"
+#include "time_exti.h"
+#include "math.h"
+
+#include "HardwareSerial.h"
+#include "SPI.h"
+#include "Wire.h"
+#include "WString.h"
+#include "Stream.h"
+
+#include "C_call_CPP.h"
+
+#endif
