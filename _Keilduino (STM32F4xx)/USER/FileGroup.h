291e09412889519decb3ce4a5266f3ddaec03037
_VIFEXTech <1290176185@qq.com>
2018-08-07 18:02:41 +0800

    Add files via upload

---
+++ _Keilduino (STM32F4xx)/USER/FileGroup.h
@@ -0,0 +1,27 @@
+#ifndef __FILEGROUP_H
+#define	__FILEGROUP_H
+
+//*************C/C++ Library**************//
+
+#include "stm32f4xx.h"
+#include "Arduino.h"
+#include "delay.h"
+#include "exti.h"
+#include "gpio.h"
+#include "adc.h"
+#include "pwm.h"
+#include "usart.h"
+#include "time_exti.h"
+#include "math.h"
+#include "WMath.h"
+#include "Tone.h"
+
+#include "HardwareSerial.h"
+#include "SPI.h"
+#include "Wire.h"
+#include "WString.h"
+#include "Stream.h"
+
+#include "C_call_CPP.h"
+
+#endif
---
+++ _Keilduino (STM32F4xx)/USER/RTE/_Project/RTE_Components.h
@@ -0,0 +1,37 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Pro' 
+ * Target:  'Project' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_DEVICE_STARTUP_STM32F4xx    /* Device Startup for STM32F4 */
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_DCMI
+#define RTE_DEVICE_STDPERIPH_DMA
+#define RTE_DEVICE_STDPERIPH_DMA2D
+#define RTE_DEVICE_STDPERIPH_EXTI
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FMC
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_I2C
+#define RTE_DEVICE_STDPERIPH_LTDC
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_SPI
+#define RTE_DEVICE_STDPERIPH_SYSCFG
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+
+#endif /* RTE_COMPONENTS_H */
