7164498f5b18a29b79a22061eb62d88060b29d2f
_VIFEXTech <1290176185@qq.com>
2018-08-14 20:38:39 +0800

    Update FileGroup.h

--- _Keilduino (STM32F0xx)/USER/FileGroup.h
+++ _Keilduino (STM32F0xx)/USER/FileGroup.h
@@ -1,5 +1,5 @@
 #ifndef __FILEGROUP_H
-#define	__FILEGROUP_H
+#define __FILEGROUP_H
 
 //*************C/C++ Library**************//
 
@@ -13,6 +13,8 @@
 #include "usart.h"
 #include "time_exti.h"
 #include "math.h"
+#include "WMath.h"
+#include "Tone.h"
 
 #include "HardwareSerial.h"
 #include "SPI.h"
