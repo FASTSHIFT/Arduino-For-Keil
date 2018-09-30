40d59df721c2aa146ff19836f55cc476815cdae8
_VIFEXTech <1290176185@qq.com>
2018-09-30 23:29:29 +0800

    Add files via upload

--- _Keilduino (STM32F10x)/Example/PWM_Output.cpp
+++ _Keilduino (STM32F10x)/Example/PWM_Output.cpp
@@ -1,15 +1,17 @@
 #include "FileGroup.h"
 
+#define PWM_Pin PA0
+
 void setup()
 {
-	PWM_Init(PA0,1000,10000);//PWM DutyCycle:0~1000 -> 0~100% , PWM Frequency: 10KHz
+	PWM_Init(PWM_Pin,1000,10000);//PWM DutyCycle:0~1000 -> 0~100% , PWM Frequency: 10KHz
 }
 
 void loop()
 {
 	for(int i = 0; i <= 1000; i++)
 	{
-		pwmWrite(PA0,i);
+		pwmWrite(PWM_Pin,i);
 		//or use analogWrite(Pin,DutyCycle);
 		delay(1);
 	}
