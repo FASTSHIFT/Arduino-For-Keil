18aa63281b778661904ae6e115fad2073060eaa0
_VIFEXTech <1290176185@qq.com>
2018-11-28 20:18:47 +0800

    Add files via upload

---
+++ _Keilduino (STM32F3xx)/USER/C_call_CPP.h
@@ -0,0 +1,14 @@
+#ifndef __C_CALL_CPP_H
+#define	__C_CALL_CPP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//Place "C" functions declaration
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ _Keilduino (STM32F3xx)/USER/FileGroup.h
@@ -0,0 +1,28 @@
+#ifndef __FILEGROUP_H
+#define	__FILEGROUP_H
+
+/** 
+  *@Illustrate: A Cross-platform migration Project | Arduino for STM32F30x 
+  *@Author: FASTSHIFT
+  *@Website: https://github.com/FASTSHIFT/Arduino-For-Keil
+  */
+
+//************* Arduino Library **************//
+#include "Arduino.h"
+#include "HardwareSerial.h"
+#include "Stream.h"
+#include "SPI.h"
+#include "WString.h"
+#include "Wire.h"
+#include "Tone.h"
+#include "WMath.h"
+
+//************* STM32 Core Library **************//
+#include "delay.h"
+#include "exti.h"
+#include "gpio.h"
+//#include "adc.h"
+#include "pwm.h"
+#include "timer.h"
+
+#endif
