b2fb2211a8258c0b98aac916bdf49646af700924
_VIFEXTech <1290176185@qq.com>
2018-11-28 20:20:22 +0800

    Add files via upload

---
+++ _Keilduino (STM32F3xx)/ArduinoAPI/Tone.h
@@ -0,0 +1,19 @@
+﻿#ifndef __TONE_H
+#define __TONE_H
+
+#include "Arduino.h"
+#include "timer.h"
+
+#define ToneVolumn_Default 80
+#define ToneTimer_Default TIM1
+
+void toneSetTimer(TIM_TypeDef* TIMx);
+void tone(uint8_t Pin, uint32_t freq, uint32_t Time_ms);
+void tone(uint8_t Pin, uint32_t freq);
+void noTone(uint8_t Pin);
+
+void toneBlock(uint8_t Pin, uint32_t freq, uint32_t Time_ms);
+void toneBlock_Volumn(uint8_t Pin, uint32_t freq, uint32_t Time_ms, uint32_t vol);
+void toneBlock_Volumn_us(uint8_t Pin, uint32_t freq, uint32_t Time_us, uint32_t vol);
+
+#endif
---
+++ _Keilduino (STM32F3xx)/ArduinoAPI/WProgram.h
@@ -0,0 +1,6 @@
+﻿#ifndef __WPROGRAM_H
+#define	__WPROGRAM_H
+
+#include "Arduino.h"
+
+#endif
