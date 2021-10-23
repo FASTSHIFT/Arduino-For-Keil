e68480f5a74c5152c0896f60acac92c51849465d
_VIFEXTech <1290176185@qq.com>
2021-10-23 21:35:10 +0800

    Update main.cpp and mcu_config_tempalte.h

--- _Keilduino (AT32F4xx)/Core/mcu_config_template.h
+++ _Keilduino (AT32F4xx)/Core/mcu_config_template.h
@@ -24,31 +24,47 @@
 #define __MCU_CONFIG_H
 
 /*=========================
-   MCU Core configuration
+   MCU configuration
  *=========================*/
 
 /* System tick */
-#define SYSTICK_TICK_FREQ           1000U // Hz
+#define SYSTICK_TICK_FREQ                   1000U // Hz
+#define SYSTICK_PRIORITY                    0
 
-/* HardwareSerial */
-#define SERIAL_USART1_ENABLE        1
-#define SERIAL_USART2_ENABLE        1
-#define SERIAL_USART3_ENABLE        1
-#define SERIAL_RX_BUFFER_SIZE       128
+/* Hardware Serial */
+#define SERIAL_USART1_ENABLE                1
+#define SERIAL_USART2_ENABLE                1
+#define SERIAL_USART3_ENABLE                1
+#define SERIAL_RX_BUFFER_SIZE               128
+#define SERIAL_PREEMPTIONPRIORITY_DEFAULT   1
+#define SERIAL_SUBPRIORITY_DEFAULT          3
 
 /* Wire (Software I2C) */
-#define WIRE_USE_FULL_SPEED_I2C     0
-#define WIRE_SDA_PIN                PB7
-#define WIRE_SCL_PIN                PB6
-#define WIRE_BEGIN_TIMEOUT          100 // ms
-#define WIRE_BUFF_SIZE              32
+#define WIRE_USE_FULL_SPEED_I2C             0
+#define WIRE_SDA_PIN                        PB7
+#define WIRE_SCL_PIN                        PB6
+#define WIRE_BEGIN_TIMEOUT                  100 // ms
+#define WIRE_BUFF_SIZE                      32
 
 /* WString memory allocator */
-#define WSTRING_MEM_CUSTOM          0
+#define WSTRING_MEM_CUSTOM                  0
 #if WSTRING_MEM_CUSTOM
-#  define WSTRING_MEM_INCLUDE       <stdlib.h>
-#  define WSTRING_MEM_REALLOC       realloc
-#  define WSTRING_MEM_FREE          free
+#  define WSTRING_MEM_INCLUDE               <stdlib.h>
+#  define WSTRING_MEM_REALLOC               realloc
+#  define WSTRING_MEM_FREE                  free
 #endif
+
+/* EXTI Interrupt priority  */
+#define EXTI_PREEMPTIONPRIORITY_DEFAULT     2
+#define EXTI_SUBPRIORITY_DEFAULT            1
+
+/* Timer Interrupt priority  */
+#define TIMER_PREEMPTIONPRIORITY_DEFAULT    0
+#define TIMER_SUBPRIORITY_DEFAULT           3
+
+/* Tone Interrupt priority  */
+#define TONE_TIMER_DEFAULT                  TIM1
+#define TONE_PREEMPTIONPRIORITY_DEFAULT     0
+#define TONE_SUBPRIORITY_DEFAULT            3
 
 #endif
