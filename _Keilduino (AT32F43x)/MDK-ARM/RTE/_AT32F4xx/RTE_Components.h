54362b206219ae4fe7e0e38780f6bb07e1008b10
_VIFEXTech <1290176185@qq.com>
2021-12-02 00:47:12 +0800

    添加 AT32F43x 系列支持
    移植gpio和delay

---
+++ _Keilduino (AT32F43x)/MDK-ARM/RTE/_AT32F4xx/RTE_Components.h
@@ -0,0 +1,29 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'proj' 
+ * Target:  'AT32F4xx' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "at32f435_437.h"
+
+#define RTE_DEVICE_STDPERIPHERALS_ADC
+#define RTE_DEVICE_STDPERIPHERALS_CRM
+#define RTE_DEVICE_STDPERIPHERALS_DMA
+#define RTE_DEVICE_STDPERIPHERALS_EXINT
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+#define RTE_DEVICE_STDPERIPHERALS_SPI
+#define RTE_DEVICE_STDPERIPHERALS_TMR
+#define RTE_DEVICE_STDPERIPHERALS_USART
+
+#endif /* RTE_COMPONENTS_H */
---
+++ _Keilduino (AT32F43x)/README.md
@@ -0,0 +1,6 @@
+# Arduino-For-Keil AT32F43x
+> https://github.com/FASTSHIFT/Arduino-For-Keil
+
+# 更新日志
+## [v1.0] - 2021.12.2
+* 1.基于AT32F43x标准外设库(ArteryTek.AT32F435_437_DFP.2.0.1)，移植了gpio和delay
